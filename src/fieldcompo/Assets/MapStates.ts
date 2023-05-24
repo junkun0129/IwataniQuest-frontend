2a00238044974bc0f3df7f7e0361bc9d0137af93
unknown <penpenetai@icloud.com>
2023-05-24 14:46:22 -0700

    created mapstate structures

---
+++ src/fieldcompo/Assets/MapStates.ts
@@ -0,0 +1,13 @@
+export const mapStatesList = {
+  firstVillage: {
+    land: "firstVillage-land",
+  },
+  bigcity: {
+    land: "bigcity-land",
+    inn: "bigcity-inn",
+    house1: "bigcity-house1",
+  },
+  outfield: {
+    land: "outfiled-land",
+  },
+};
