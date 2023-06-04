71a8b59691e9ef4f99d2d671eff8605fc3303e91
unknown <penpenetai@icloud.com>
2023-06-03 22:00:17 -0700

    removed gp

--- src/fieldcompo/npc/Npc.ts
+++ src/fieldcompo/npc/Npc.ts
@@ -1,59 +1,44 @@
 import { GamePanel } from "../GamePanel.js";
 import { Entity } from "./Entity.js";
 
-export class Npc extends Entity{
-    public gp: GamePanel;
+export class Npc extends Entity {
+  constructor(gp: GamePanel) {
+    super(gp);
+    this.gp = gp;
+  }
 
-    
-    
+  speak(): void {
+    // let rock = this.gp.asset.itemsOnMap.findIndex(array => array.name === "rock");
 
-    constructor(gp:GamePanel){
-        super(gp);
-        this.gp = gp;
-        
+    // if(rock !== -1){
 
+    //     if(this.dialogIndex === 4){
+
+    //         this.gp.gameState = this.gp.fieldScene;
+    //         this.dialogIndex = 0;
+    //     }
+
+    //     this.gp.ui.currentDialog = this.dialogs[this.dialogIndex];
+    // }
+    // if(rock === -1){
+    //     if(this.dialogIndex === 0){
+    //         this.dialogIndex = 5;
+    //     }
+
+    //     if(this.dialogIndex === 10){
+
+    //         this.gp.gameState = this.gp.fieldScene;
+    //         this.dialogIndex = 0;
+    //     }
+    //     this.gp.ui.currentDialog = this.dialogs[this.dialogIndex];
+    // }
+
+    const maxDialogIndex = this.dialogs.length;
+
+    if (this.dialogIndex === maxDialogIndex) {
+      this.gp.gameState = this.gp.fieldScene;
+      this.dialogIndex = 0;
     }
-
-    
-    speak(): void {
-        // let rock = this.gp.asset.itemsOnMap.findIndex(array => array.name === "rock");
-
-        // if(rock !== -1){
-            
-            
-        //     if(this.dialogIndex === 4){
-                
-        //         this.gp.gameState = this.gp.fieldScene;
-        //         this.dialogIndex = 0;
-        //     }
-
-        //     this.gp.ui.currentDialog = this.dialogs[this.dialogIndex];
-        // }
-        // if(rock === -1){
-        //     if(this.dialogIndex === 0){
-        //         this.dialogIndex = 5;
-        //     }
-           
-        //     if(this.dialogIndex === 10){
-                
-        //         this.gp.gameState = this.gp.fieldScene;
-        //         this.dialogIndex = 0;
-        //     }
-        //     this.gp.ui.currentDialog = this.dialogs[this.dialogIndex];
-        // }
-
-        const maxDialogIndex = this.dialogs.length;
-
-        if(this.dialogIndex === maxDialogIndex){
-            this.gp.gameState = this.gp.fieldScene;
-            this.dialogIndex =0;
-        }
-        this.gp.ui.currentDialog = this.dialogs[this.dialogIndex];
-    
-       
-    }
-
-    
-
-
+    this.gp.ui.currentDialog = this.dialogs[this.dialogIndex];
+  }
 }
